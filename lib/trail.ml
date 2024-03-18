3131e2726a76c8337f34ebb6ec3af5519354505e
jeroen noels <jeroen.noels@telenet.be>
2024-03-18 22:37:54 +0100

    rename

--- lib/trail.ml
+++ lib/trail.ml
@@ -14,8 +14,8 @@ let negate = function
 (* all our arrays below waste index zero to avoid offsetting *)
 
 type t =
-  { steps_to_vars : int array
-  ; vars_to_steps : int array
+  { step_to_var : int array
+  ; var_to_step : int array
   ; mutable length : int
   ; mutable last_decision_step : int option (* option until we handle backjumps *)
   ; mutable log : bool
@@ -23,21 +23,21 @@ type t =
 
 let create ~nbvar =
   let len = nbvar + 1 in
-  let steps_to_vars = Array.create ~len 0 in
-  let vars_to_steps = Array.create ~len 0 in
+  let step_to_var = Array.create ~len 0 in
+  let var_to_step = Array.create ~len 0 in
   for i = 1 to nbvar do
-    steps_to_vars.(i) <- i;
-    vars_to_steps.(i) <- i
+    step_to_var.(i) <- i;
+    var_to_step.(i) <- i
   done;
-  { steps_to_vars; vars_to_steps; length = 0; last_decision_step = None; log = false }
+  { step_to_var; var_to_step; length = 0; last_decision_step = None; log = false }
 ;;
 
-let num_variables (t : t) = Array.length t.vars_to_steps - 1
+let num_variables (t : t) = Array.length t.var_to_step - 1
 let is_complete (t : t) = t.length = num_variables t
 
 let invariant (t : t) =
-  let f v s = t.steps_to_vars.(Int.abs s) = v in
-  Array.for_alli ~f t.vars_to_steps
+  let f v s = t.step_to_var.(Int.abs s) = v in
+  Array.for_alli ~f t.var_to_step
 ;;
 
 let step_internal (t : t) x =
@@ -47,14 +47,14 @@ let step_internal (t : t) x =
   let i = t.length + 1 in
   t.length <- i;
   let v = Variable.to_int var in
-  let j = Int.abs t.vars_to_steps.(v) in
+  let j = Int.abs t.var_to_step.(v) in
   if j < i then invalid_arg "variable is already assigned";
-  let w = t.steps_to_vars.(i) in
+  let w = t.step_to_var.(i) in
   (* watch out for the special case i = j and v = w *)
-  t.steps_to_vars.(i) <- v;
-  t.steps_to_vars.(j) <- w;
-  t.vars_to_steps.(v) <- (if pos then i else -i);
-  t.vars_to_steps.(w) <- (if pos then j else -j)
+  t.step_to_var.(i) <- v;
+  t.step_to_var.(j) <- w;
+  t.var_to_step.(v) <- (if pos then i else -i);
+  t.var_to_step.(w) <- (if pos then j else -j)
 ;;
 
 let step t (x, _) =
@@ -77,7 +77,7 @@ let backjump (t : t) ~length =
 
 let eval_variable (t : t) var =
   let v = Variable.to_int var in
-  let i = t.vars_to_steps.(v) in
+  let i = t.var_to_step.(v) in
   if i > 0
   then if i > t.length then Undefined else True
   else if -i > t.length
@@ -100,7 +100,7 @@ let random_unassigned_exn (t : t) =
   let nbvar = num_variables t in
   (* raises when bounds cross *)
   let i = Random.int_incl (t.length + 1) nbvar in
-  Variable.of_int_check ~nbvar t.steps_to_vars.(i)
+  Variable.of_int_check ~nbvar t.step_to_var.(i)
 ;;
 
 let iter_down_until_last_decision (t : t) ~f =
@@ -111,7 +111,7 @@ let iter_down_until_last_decision (t : t) ~f =
     let rec go i =
       if i > decision_step
       then (
-        let var = Variable.of_int_unchecked t.steps_to_vars.(i) in
+        let var = Variable.of_int_unchecked t.step_to_var.(i) in
         f var;
         go (i - 1))
     in
@@ -121,7 +121,7 @@ let iter_down_until_last_decision (t : t) ~f =
 let copy_unassigned (t : t) =
   let nbvar = num_variables t in
   let pos = t.length + 1 in
-  let copy = Array.subo t.steps_to_vars ~pos in
+  let copy = Array.subo t.step_to_var ~pos in
   Array.sort ~compare:Int.compare copy;
   Array.map ~f:(Variable.of_int_check ~nbvar) copy
 ;;
