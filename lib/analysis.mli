6cfac83b75a470588ec4ee4f307cf2e7f1666280
jeroen noels <jeroen.noels@telenet.be>
2024-03-25 21:22:05 +0100

    undo previous commit

--- lib/analysis.mli
+++ lib/analysis.mli
@@ -3,7 +3,7 @@ open! Base
 type t
 
 val analyze_conflict : Database.t -> Trail.t -> Conflict.t -> t
-val get_conflict_clause : t -> Literal.t array
+val get_learned_clause : t -> Literal.t array
 
 (* exposed for testing and debugging *)
 val get_conflict_variable : t -> Variable.t
